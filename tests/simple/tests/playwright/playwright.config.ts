2633c45b2ab48021641fc12c5f5d7d7a4f19af6d
Pierre Chalamet <pierre@magnusopera.io>
2025-08-01 23:09:43 +0200

    back to chromium

--- tests/simple/tests/playwright/playwright.config.ts
+++ tests/simple/tests/playwright/playwright.config.ts
@@ -1,18 +1,11 @@
 import { defineConfig } from '@playwright/test'
 
 export default defineConfig({
-  use: {
-    headless: true,
-    launchOptions: {
-      args: ['--no-sandbox']
-    }
-  },
-  workers: 1,
   reporter: [['junit', { outputFile: 'test-results/junit.xml' }]],
-    projects: [
+  projects: [
     {
-        name: 'firefox',
-        use: { browserName: 'firefox' }
+        name: 'chromium',
+        use: { browserName: 'chromium' }
     }
   ]
 })
