8b358f07629580734eb170783e76640c028c5195
Pierre Chalamet <pierre@magnusopera.io>
2025-08-03 08:14:25 +0200

    fix cargo test

--- src/Terrabuild.Extensions.Tests/Helpers/Cargo.fs
+++ src/Terrabuild.Extensions.Tests/Helpers/Cargo.fs
@@ -6,11 +6,12 @@ open FsUnit
 
 [<Test>]
 let ``find project file``() =
-    NpmHelpers.findProjectFile "TestFiles/cargo-app"
+    CargoHelpers.findProjectFile "TestFiles/cargo-app"
     |> should equal "TestFiles/cargo-app/Cargo.toml"
 
 [<Test>]
 let ``find dependencies``() =
-    NpmHelpers.findDependencies "TestFiles/cargo-app/Cargo.toml"
-    |> should equal (Set [])
+    // TODO: fix implementation
+    CargoHelpers.findDependencies "TestFiles/cargo-app/Cargo.toml"
+    |> should be Empty
 
