3c8b40d98a128766bc8bb369ca1d74d8c70c597c
Pierre Chalamet <pierre@magnusopera.io>
2025-09-20 23:38:25 +0200

    fix merge root nodes

--- src/Terrabuild/Core/ClusterBuilder.fs
+++ src/Terrabuild/Core/ClusterBuilder.fs
@@ -1,12 +1,12 @@
 
 module ClusterBuilder
 open GraphDef
+open Collections
 
 
-type Cluster = {
-    Id: string
-    Nodes: Set<string>
-}
+type Cluster =
+    { Id: string
+      Nodes: Set<string> }
 
 let computeClusters (graph: Graph) =
     let mutable clusterIdCounter = 0
@@ -14,38 +14,45 @@ let computeClusters (graph: Graph) =
         clusterIdCounter <- clusterIdCounter + 1
         $"cluster-{clusterIdCounter}"
 
-    let nodeToCluster = System.Collections.Generic.Dictionary<string, string>()
-    let clusters = System.Collections.Generic.Dictionary<string, System.Collections.Generic.HashSet<string>>()
+    let mutable nodeToCluster = Map.empty
+    let mutable clusters = Map.empty
 
-    let rec visit (nodeId: string) =
-        if not (nodeToCluster.ContainsKey nodeId) then
-            let node = graph.Nodes[nodeId]
+    let merge cid dependencies clusterHash =
+        let mutable cluster = Set.empty
+        for depId in dependencies do
+            let depNode = graph.Nodes[depId]
+            let cidDep = nodeToCluster[depId]
+            if depNode.ClusterHash = clusterHash && cid <> cidDep then
+                for nid in clusters[cidDep] do
+                    nodeToCluster <- nodeToCluster |> Map.add nid cid
+                    cluster <- cluster |> Set.add nid
+                clusters <- clusters |> Map.remove cidDep
+        cluster
 
+    let rec visit nodeId =
+        let node = graph.Nodes[nodeId]
+        if not (nodeToCluster.ContainsKey nodeId) && node.Action = NodeAction.Build then
             for depId in node.Dependencies do
                 visit depId
-
             let cid = nextClusterId()
-            nodeToCluster[nodeId] <- cid
-            clusters[cid] <- System.Collections.Generic.HashSet<string>([ nodeId ])
-            for depId in node.Dependencies do
-                visit depId
+            let cluster = merge cid node.Dependencies node.ClusterHash |> Set.add nodeId
+            nodeToCluster <- nodeToCluster |> Map.add nodeId cid
+            clusters <- clusters |> Map.add cid cluster
 
-                let depNode = graph.Nodes[depId]
-                let cidDep = nodeToCluster[depId]
-                if depNode.ClusterHash = node.ClusterHash && cid <> cidDep then
-                    for nid in clusters[cidDep] do
-                        nodeToCluster[nid] <- cid
-                        clusters[cid].Add nid |> ignore
-                    clusters.Remove cidDep |> ignore
-
-    for root in graph.RootNodes do
-        visit root
+    for root in graph.RootNodes do visit root
+    let rootHashs = graph.RootNodes |> Set.map (fun rootId -> graph.Nodes[rootId].ClusterHash)
+    for rootHash in rootHashs do
+        let cid = nextClusterId()
+        let cluster = merge cid graph.RootNodes rootHash
+        nodeToCluster <- nodeToCluster |> Map.add cid cid
+        clusters <- clusters |> Map.add cid cluster
 
     // Build final immutable representation
     let clusters =
         clusters
         |> Seq.map (fun kvp -> { Id = kvp.Key; Nodes = Set.ofSeq kvp.Value })
         |> Seq.toList
+    // clusters
 
     printfn $"All clusters:"
     for cluster in clusters do
