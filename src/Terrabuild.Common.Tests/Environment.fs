c58f2c4c9c403d1f5d06792f7d5b29f1164b9baf
Pierre Chalamet <pierre@magnusopera.io>
2025-11-28 08:11:23 +0100

    cleanup unit tests

--- src/Terrabuild.Common.Tests/Environment.fs
+++ src/Terrabuild.Common.Tests/Environment.fs
@@ -2,47 +2,45 @@ module Tests.Environment
 open FsUnit
 open NUnit.Framework
 
-let getVarName() =
+let getVarName prefix =
     let rnd = System.Random()
-    $"tagada{rnd.Next()}"
+    let name = $"tagada{rnd.Next()}"
+    name, $"{prefix}{name}"
 
 [<Test>]
 let ``no variable returns None``() =
-    let name = getVarName()
-
+    let name, _ = getVarName "TB_VAR"
     name
     |> Environment.getTerrabuildEnvVar
     |> should equal None
 
 [<Test>]
 let ``not matching prefix returns None``() =
-    let name = getVarName()
-    System.Environment.SetEnvironmentVariable($"TB_var_{name}", "pouet pouet")
+    let name, tbvar = getVarName "TB_var_"
 
+    System.Environment.SetEnvironmentVariable(tbvar, "pouet pouet")
     name
     |> Environment.getTerrabuildEnvVar
     |> should equal None
-
-    System.Environment.SetEnvironmentVariable($"TB_var_{name}", null)
+    System.Environment.SetEnvironmentVariable(tbvar, null)
 
 [<Test>]
 let ``matching case returns Some``() =
-    let name = getVarName()
-    System.Environment.SetEnvironmentVariable($"TB_VAR_{name}", "pouet pouet")
+    let name, tbvar = getVarName "TB_VAR_"
 
+    System.Environment.SetEnvironmentVariable(tbvar, "pouet pouet")
     name
     |> Environment.getTerrabuildEnvVar
     |> should equal (Some "pouet pouet")
-
-    System.Environment.SetEnvironmentVariable($"TB_var_{name}", null)
+    System.Environment.SetEnvironmentVariable(tbvar, null)
 
 [<Test>]
 let ``not matching case returns Some``() =
-    let name = getVarName() |> String.toUpper
-    System.Environment.SetEnvironmentVariable($"TB_VAR_{name}", "pouet pouet")
+    let name, tbvar = getVarName "TB_VAR_"
+    let name = name |> String.toUpper
 
+    System.Environment.SetEnvironmentVariable(tbvar, "pouet pouet")
     name
     |> Environment.getTerrabuildEnvVar
     |> should equal (Some "pouet pouet")
-
-    System.Environment.SetEnvironmentVariable($"TB_var_{name}", null)
+    System.Environment.SetEnvironmentVariable(tbvar, null)
