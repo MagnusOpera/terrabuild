185afdfa8253a817e0690ca9eee9f241dfdbbee1
Pierre Chalamet <pierre@magnusopera.io>
2025-11-30 16:18:35 +0100

    revert non required change

--- src/Terrabuild/Storages/Factory.fs
+++ src/Terrabuild/Storages/Factory.fs
@@ -1,6 +1,6 @@
 module Storages.Factory
 
-let create api : Contracts.IStorage =
+let create api: Contracts.IStorage =
     match api with
     | None -> Local()
     | Some api -> AzureBlobStorage(api)
